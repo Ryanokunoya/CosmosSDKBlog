a4a19abc9f71449154f3235f824cf00328f209d8
Yunchen Zhu <yunchez3@uci.edu>
2021-04-27 17:32:57 -0700

    first commit

--- x/blog/handler.go
+++ x/blog/handler.go
@@ -16,11 +16,13 @@ func NewHandler(k keeper.Keeper) sdk.Handler {
 	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
 		ctx = ctx.WithEventManager(sdk.NewEventManager())
 
+		// x/blog/handler.go
+
 		switch msg := msg.(type) {
-		// this line is used by starport scaffolding # 1
-		default:
-			errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
-			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, errMsg)
-		}
+			// this line is used by starport scaffolding # 1
+			case *types.MsgCreatePost:
+				return handleMsgCreatePost(ctx, k, msg)
+			default:
+	
 	}
 }
